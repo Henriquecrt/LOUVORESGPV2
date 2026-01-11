ad017e374ce2bb3ca886eac2ad744b008943557c
Henriquecrt <henriquebpmteste@gmail.com>
2026-01-11 00:39:09 -0300

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,34 +1,22 @@
+
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './src/app.component'; 
-import { provideRouter } from '@angular/router';
-import { routes } from './src/app.routes'; 
-import { provideHttpClient, withJsonpSupport } from '@angular/common/http';
-
-// Importações do Firebase
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-// 👇 NOVA IMPORTAÇÃO DO AUTH 👇
-import { getAuth, provideAuth } from '@angular/fire/auth'; 
-
-// Configuração Direta
-const firebaseConfig = {
-  apiKey: "AIzaSyCb4xuxWrCGNuFM8yAFVPXvI7K35LX9WCE",
-  authDomain: "louvores-gpv.firebaseapp.com",
-  projectId: "louvores-gpv",
-  storageBucket: "louvores-gpv.firebasestorage.app",
-  messagingSenderId: "876395905450",
-  appId: "1:876395905450:web:deba8f7763c562aec42bd3"
-};
+import { AppComponent } from './src/app.component';
+import { provideZonelessChangeDetection, isDevMode } from '@angular/core';
+import { provideRouter, withHashLocation } from '@angular/router';
+import { routes } from './src/app.routes';
+import { provideServiceWorker } from '@angular/service-worker';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes),
-    provideHttpClient(withJsonpSupport()), 
-    
-    // Inicializa Firebase + Banco + Auth
-    provideFirebaseApp(() => initializeApp(firebaseConfig)),
-    provideFirestore(() => getFirestore()),
-    // 👇 LIGANDO O SISTEMA DE LOGIN 👇
-    provideAuth(() => getAuth()) 
+    provideZonelessChangeDetection(),
+    provideRouter(routes, withHashLocation()), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
   ]
 }).catch((err) => console.error(err));
+
+// AI Studio always uses an `index.tsx` file for all project types.
